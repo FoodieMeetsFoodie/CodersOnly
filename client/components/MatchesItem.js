719524c84650d60256006100f03765b19001c5da
rebshess <rebeccashesser@gmail.com>
2022-10-25 19:59:32 -0400

    added module for chat

--- client/components/MatchesItem.js
+++ client/components/MatchesItem.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import '../stylesheets/MatchesItem.css';
+import { useState } from 'react';
+import CollabBox from './CollabBox';
 
 const MatchesItem = (props) => {
+  //react hook for chat module
+  const [buttonPopup, setButtonPopup] = useState(false);
   // The way each user profile will look in the feed
   if (!props.user) {
     return <p>You have no matches xD</p>;
   }
   const { username, age, location, comment, proglang, url } = props.user;
   return (
-    <div className='matchesContainer'>
-      <div className='username'>
-        <h3 id='userName'>{username}</h3>
+    <div className="matchesContainer">
+      <div className="username">
+        <h3 id="userName">{username}</h3>
       </div>
-      <img className='matchesImage' src={url} alt='profileImage' />
-      <p className='userDetail'>Age: {age}</p>
-      <p className='userDetail'>Location: {location}</p>
-      <p className='userDetail'>Bio: {comment}</p>
-      <p className='userDetail'>Programming Language: {proglang}</p>
-      <button>slideInto{username}DMs()</button>
+      <img className="matchesImage" src={url} alt="profileImage" />
+      <p className="userDetail">Age: {age}</p>
+      <p className="userDetail">Location: {location}</p>
+      <p className="userDetail">Bio: {comment}</p>
+      <p className="userDetail">Programming Language: {proglang}</p>
+      <button className="profile-btn" onClick={() => setButtonPopup(true)}>
+        slideInto{username}DMs()
+      </button>
+      <CollabBox trigger={buttonPopup} setButtonPopup={setButtonPopup} />
     </div>
   );
 };
