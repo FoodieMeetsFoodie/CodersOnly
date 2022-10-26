105e189b6776e11d2c32e7bc39cd6ba0d619b929
Jordan Jeter <jordan@Jordans-MacBook-Pro.local>
2022-10-26 14:44:47 -0400

    move yes/no button into feeditem

--- client/components/FeedItem.js
+++ client/components/FeedItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../stylesheets/FeedItem.css';
+import Button from './Button';
 
 const FeedItem = (props) => {
   //the way each user profile will look in the feed
@@ -17,6 +18,10 @@ const FeedItem = (props) => {
       <p className='userDetail'>Location: {location}</p>
       <p className='userDetail'>Bio: {comment}</p>
       <p className='userDetail'>Programming Language: {proglang}</p>
+      <div id='matchButtons'>
+      <Button text='yes' onClick={props.yes} />
+      <Button text='no' onClick={props.no} />
+      </div>
     </div>
   );
 };
